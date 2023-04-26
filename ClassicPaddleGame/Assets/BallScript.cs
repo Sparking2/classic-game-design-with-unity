768a15471ddea37db9dd437d2f8751bbcc6c0e86
Sparking2 <sparking02@yahoo.com.mx>
2023-04-25 19:32:42 -0600

    v0.03

---
+++ ClassicPaddleGame/Assets/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+        private void Start()
+        {
+                Rigidbody rigidb = GetComponent<Rigidbody>();
+                if ( rigidb )
+                {
+                        rigidb.freezeRotation = true;
+                }
+
+                StartCoroutine(nameof( WaitForIt ));
+        }
+
+        private void Update()
+        {
+        }
+
+        private IEnumerator WaitForIt()
+        {
+                Rigidbody rigidb = GetComponent<Rigidbody>();
+                yield return new WaitForSeconds(3);
+                if ( rigidb )
+                {
+                        rigidb.AddForce(UnityEngine.Random.Range(6,0),UnityEngine.Random.Range(-4,-3),0);
+                }
+        }
+}
