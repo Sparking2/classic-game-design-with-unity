e6badd366f77a8425a11fdc6aa07ad62437818a0
Sparking2 <sparking02@yahoo.com.mx>
2023-04-25 19:16:05 -0600

    🎨 style: format code in ClassicPaddleGame/Assets directory ✨ feat: add P1.cs and P2.cs scripts for player movement 🆕 feat: add Mat_Paddle material for paddle appearance
    🔨 refactor(SampleScene.unity): add PaddleLeft and PaddleRight game objects with BoxCollider, MeshRenderer, MeshFilter, and MonoBehaviour components.

---
+++ ClassicPaddleGame/Assets/P1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class P1 : MonoBehaviour
+{
+        private void Update()
+        {
+                if ( Input.GetKey("w") )
+                {
+                        transform.Translate(0,20*Time.deltaTime,0);
+                }
+
+                if ( Input.GetKey("s") )
+                {
+                        transform.Translate(0,-20*Time.deltaTime,0);
+                }
+        }
+}
---
+++ ClassicPaddleGame/Assets/P2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class P2 : MonoBehaviour
+{
+        private void Update()
+        {
+                if ( Input.GetKey("up") )
+                {
+                        transform.Translate(0,20*Time.deltaTime,0);
+                }
+
+                if ( Input.GetKey("down") )
+                {
+                        transform.Translate(0,-20*Time.deltaTime,0);
+                }
+        }
+}
