ef15ff1cf33ca17f948e81dcdb6c71c95dffd269
Sparking2 <sparking02@yahoo.com.mx>
2023-04-27 19:07:46 -0600

    🚀 feat(BallRelaunch.cs): add BallRelaunch script to reset ball position on trigger enter event

---
+++ ClassicPaddleGame/Assets/BallRelaunch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class BallRelaunch : MonoBehaviour
+{
+        private void OnTriggerEnter( Collider other )
+        {
+                other.transform.position = new Vector3(0, 0, 0);
+        }
+}
