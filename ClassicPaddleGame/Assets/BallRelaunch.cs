eea90c79621ce2ad157a42583c84af34d36479ce
Sparking2 <sparking02@yahoo.com.mx>
2023-04-29 14:29:35 -0600

    🎵 feat: add pluck sound effect to ball collision 🔊 chore: add AudioSource component to BallScript 🚀 chore: update workspace.xml to include pluck.wav and BallScript.cs changes

--- ClassicPaddleGame/Assets/BallRelaunch.cs
+++ ClassicPaddleGame/Assets/BallRelaunch.cs
@@ -5,6 +5,11 @@ public class BallRelaunch : MonoBehaviour
 {
         private void OnTriggerEnter( Collider other )
         {
+                if ( other.transform.position.x > 0 )
+                        Scoring.scorep1++;
+                else
+                        Scoring.scorep2++;
+                
                 other.transform.position = new Vector3(0, 0, 0);
         }
 }
---
+++ ClassicPaddleGame/Assets/Scoring.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+        public static int scorep1;
+        public static int scorep2;
+
+        private void Start()
+        {
+                scorep1 = 0;
+                scorep2 = 0;
+        }
+
+        private void OnGUI()
+        {
+                GUI.Box(new Rect(10,10,200,30),"Player 1 Score:" + scorep1);
+                GUI.Box(new Rect(Screen.width - 250,10,200,30), "Player 2 Score: " + scorep2);
+        }
+}
