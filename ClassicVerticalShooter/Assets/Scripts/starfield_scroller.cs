f4e96294831c3d640dc755703d023e524130db60
spark <sparking02@yahoo.com.mx>
2023-06-30 19:36:56 -0600

    ✨ Add starfield_scroller script
    This commit adds the starfield_scroller script to the project. The script
    controls the scrolling behavior of the starfield in the game. It includes a
    scrollspeed variable that determines the speed at which the starfield scrolls.
    The Update() function is responsible for translating the starfield's position
    based on the scrollspeed. If the starfield's position goes below a certain
    threshold, it wraps around to the top of the screen.

---
+++ ClassicVerticalShooter/Assets/Scripts/starfield_scroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class starfield_scroller : MonoBehaviour
+{
+    public float scrollspeed;
+
+    void Start() { }
+
+    void Update()
+    {
+        transform.Translate(0, scrollspeed * Time.deltaTime, 0);
+        if ( transform.position.y < -10.0f )
+        {
+            transform.Translate(0, 20.48f, 0);
+        }
+    }
+}
