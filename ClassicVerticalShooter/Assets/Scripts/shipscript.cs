53f100f5a877a262fcd6759cfe0c88d5998ec125
spark <sparking02@yahoo.com.mx>
2023-07-15 17:55:01 -0600

    ✨ Add shipscript and shotscript classes
    📝 This commit adds the shipscript and shotscript classes to the project. The shipscript class controls the movement of the player's ship, while the shotscript class controls the movement and destruction of shots fired by the ship. Both classes include necessary variables and functions for their respective functionalities.

---
+++ ClassicVerticalShooter/Assets/Scripts/shipscript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class shipscript : MonoBehaviour
+{
+    public float shipSpeed;
+    public float screenBoundary;
+    public GameObject shot;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, -2, 0);
+    }
+
+    void Update()
+    {
+        if ( Input.GetKey("right") )
+        {
+            transform.Translate(shipSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if ( Input.GetKey("left") )
+        {
+            transform.Translate(-shipSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if ( Input.GetKeyDown("space") )
+        {
+            Instantiate(shot, new Vector3(transform.position.x, transform.position.y, 0.5f), Quaternion.identity);
+        }
+
+        if ( transform.position.x < -screenBoundary )
+        {
+            transform.position = new Vector3(-screenBoundary, transform.position.y, transform.position.z);
+        }
+
+        if ( transform.position.x > screenBoundary )
+        {
+            transform.position = new Vector3(screenBoundary, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ ClassicVerticalShooter/Assets/Scripts/shotscript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class shotscript : MonoBehaviour
+{
+    public float shotSpeed;
+    void Start() { }
+
+    void Update()
+    {
+        transform.Translate(0, shotSpeed * Time.deltaTime, 0);
+
+        if ( transform.position.y > 6.0f ) Destroy(gameObject);
+    }
+}
