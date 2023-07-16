07654835b38a74d86d75cbbe1180f3faa489b9a0
spark <sparking02@yahoo.com.mx>
2023-07-15 18:28:52 -0600

    aliens and aliens die

---
+++ ClassicVerticalShooter/Assets/Scripts/alienfactoryscript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class alienfactoryscript : MonoBehaviour
+{
+    public GameObject alien;
+
+    public void MakeAliens()
+    {
+        for ( int i = 0; i < 15; i++ )
+        {
+            for ( int j = 0; j < 6; j++ )
+            {
+                GameObject al = Instantiate(alien, new Vector3(( i - 7 ) * 0.5f, ( j - 2 ) * 0.8f, 0),
+                    Quaternion.identity);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        MakeAliens();
+    }
+}
---
+++ ClassicVerticalShooter/Assets/Scripts/alienscript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class alienscript : MonoBehaviour
+{
+    private void OnTriggerEnter2D( Collider2D other )
+    {
+        Debug.Log("hmmmm");
+        if ( other.tag == "shot" )
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
