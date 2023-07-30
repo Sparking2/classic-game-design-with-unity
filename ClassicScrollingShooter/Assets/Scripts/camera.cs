8f0eeece02c57356790da7dcd780e873aba98550
spark <sparking02@yahoo.com.mx>
2023-07-29 19:18:54 -0600

    ✨📦 Add scrollingship.cs and camera.cs scripts
    📝 Add scrollingship.cs and camera.cs scripts to the project. These scripts
    handle the movement of the scrolling ship and the camera respectively.

---
+++ ClassicScrollingShooter/Assets/Scripts/camera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private void Start() { }
+
+    private void Update()
+    {
+        GameObject player = GameObject.Find("ScrollingShipTextured");
+        transform.position = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y,
+            transform.position.z
+        );
+    }
+}
---
+++ ClassicScrollingShooter/Assets/Scripts/scrollingship.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class scrollingship : MonoBehaviour
+{
+    void Start() { }
+
+    void Update()
+    {
+        transform.Translate(-0.3f * Time.deltaTime, 0, 0);
+        if ( Input.GetKey("w") )
+        {
+            transform.Translate(0, 0.8f * Time.deltaTime, 0);
+        }
+
+        if ( Input.GetKey("s") )
+        {
+            transform.Translate(0, -0.8f * Time.deltaTime, 0);
+        }
+    }
+}
