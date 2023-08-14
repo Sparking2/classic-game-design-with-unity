2da7172dde2db73971c12c65d54841fb97cefb7f
spark <sparking02@yahoo.com.mx>
2023-08-13 19:27:56 -0600

    0.06 Rockets

--- ClassicScrollingShooter/Assets/Scripts/camera.cs
+++ ClassicScrollingShooter/Assets/Scripts/camera.cs
@@ -8,9 +8,17 @@ public class camera : MonoBehaviour
     private void Update()
     {
         GameObject player = GameObject.Find("ScrollingShipTextured");
+
+        float xpos = player.transform.position.x;
+        float ypos = player.transform.position.y;
+
+        float new_ypos = transform.position.y;
+        if ( new_ypos < ypos - 0.5f ) new_ypos = ypos - 0.5f;
+        if ( new_ypos > ypos + 0.5f ) new_ypos = ypos + 0.5f;
+
         transform.position = new Vector3(
-            player.transform.position.x,
-            player.transform.position.y,
+            xpos - 0.7f,
+            new_ypos,
             transform.position.z
         );
     }
