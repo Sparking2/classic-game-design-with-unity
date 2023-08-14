5ff0954b20b6f214b3b25807f7c03c4db4b1c88c
spark <sparking02@yahoo.com.mx>
2023-08-13 19:47:07 -0600

    0.07, flying rockets

--- ClassicScrollingShooter/Assets/Scripts/camera.cs
+++ ClassicScrollingShooter/Assets/Scripts/camera.cs
@@ -9,17 +9,20 @@ public class camera : MonoBehaviour
     {
         GameObject player = GameObject.Find("ScrollingShipTextured");
 
-        float xpos = player.transform.position.x;
-        float ypos = player.transform.position.y;
+        if ( player )
+        {
+            float xpos = player.transform.position.x;
+            float ypos = player.transform.position.y;
 
-        float new_ypos = transform.position.y;
-        if ( new_ypos < ypos - 0.5f ) new_ypos = ypos - 0.5f;
-        if ( new_ypos > ypos + 0.5f ) new_ypos = ypos + 0.5f;
+            float new_ypos = transform.position.y;
+            if ( new_ypos < ypos - 0.5f ) new_ypos = ypos - 0.5f;
+            if ( new_ypos > ypos + 0.5f ) new_ypos = ypos + 0.5f;
 
-        transform.position = new Vector3(
-            xpos - 0.7f,
-            new_ypos,
-            transform.position.z
-        );
+            transform.position = new Vector3(
+                xpos - 0.7f,
+                new_ypos,
+                transform.position.z
+            );
+        }
     }
 }
---
+++ ClassicScrollingShooter/Assets/Scripts/rocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    public float rocketSpeed;
+
+    void Start() { }
+
+    private void Update()
+    {
+        GameObject player = GameObject.Find("ScrollingShipTextured");
+        if ( player )
+        {
+            if ( player.transform.position.x - transform.position.x < 0.5f )
+            {
+                transform.Translate(0, rocketSpeed * Time.deltaTime, 0);
+            }
+        }
+    }
+
+    private void OnTriggerEnter( Collider other )
+    {
+        if ( other.tag == "scrollingship" )
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
