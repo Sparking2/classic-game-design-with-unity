dd68aeb4ef87b5affa7e3b1d5546d55c27bcbd06
spark <sparking02@yahoo.com.mx>
2023-09-04 19:41:35 -0600

    ✨🔧 Add player movement functionality
    📝 Added player.cs script to handle player movement using Rigidbody.AddForce() method. The player can move in four directions: right, left, down, and up. The movement is controlled by the arrow keys.

---
+++ ClassicMazeGame/Assets/Scripts/player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+        void Start(){}
+
+        private float factor = 10.0f;
+
+        void Update()
+        {
+                if ( Input.GetKey("right") )
+                {
+                        GetComponent<Rigidbody>().AddForce(Vector3.right * factor);
+                }
+
+                if ( Input.GetKey("left") )
+                {
+                        GetComponent<Rigidbody>().AddForce(Vector3.left * factor);
+                }
+                if ( Input.GetKey("down") )
+                {
+                        GetComponent<Rigidbody>().AddForce(Vector3.back * factor);
+                }
+                if ( Input.GetKey("up") )
+                {
+                        GetComponent<Rigidbody>().AddForce(Vector3.forward * factor);
+                }
+        }
+}
